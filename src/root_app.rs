3ee4d687609425a8464ef9710bdb03b4341f54a8
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-05-28 15:14:56 +0300

    Update root_app.rs

--- src/root_app.rs
+++ src/root_app.rs
@@ -1,24 +1,4 @@
-//
-struct DomikView {
-    title: String,
-}
-impl Default for DomikView {
-    fn default() -> Self {
-        Self{ title: "simple DoMiKkk".to_owned() }
-    }
-}
-impl DomikView {
-    pub fn new() -> Self {
-        Default::default()
-    }
-    pub fn updateUI(&mut self, ui: &mut egui::Ui ) {
-            ui.label("WWWapp Template v2.00");
-            let btn = ui.button( "try to save TEXT" );
-            if btn.clicked(){
-                println!("clicked simple!");
-            }
-    }
-}
+
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
