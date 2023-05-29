5e2cab63edc757ca9c068c20f56f32d2807991e5
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-05-29 21:50:46 +0300

    Update dbg_utils.rs

--- src/dbg_utils.rs
+++ src/dbg_utils.rs
@@ -1,11 +1,12 @@
 use std::sync::Mutex;
 
-static interLog: Mutex<String> = Mutex::new( String::new() );
+static INTER_LOG: Mutex<String> = Mutex::new( String::new() );
+
 pub fn getInterLog() -> String {
-    let res = interLog.lock().unwrap();
+    let res = INTER_LOG.lock().unwrap();
     res.clone()
 }
 pub fn appendInterLog(str: &str){
-    let mut log = interLog.lock().unwrap();
+    let mut log = INTER_LOG.lock().unwrap();
     *log = format!( "\n{}\n{}",log,str );
 }
