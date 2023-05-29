91152248d03a6f59346f435127b60dc44549d873
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-05-29 23:28:55 +0300

    Update raadbg.rs

--- src/raadbg.rs
+++ src/raadbg.rs
@@ -29,27 +29,5 @@ fn add_log_line(line: String) {
     let mut log = INTER_LOG.lock().unwrap();
     *log += "\n";
     *log += &line;
-    *log += "\n";
     println!("{line}");
 }
-
-/*pub fn log(status: Status){
-    let newItem: String = match status {
-        Simple(msg) =>       format!("> {msg}"),
-        New(name) =>         format!("[+ {name}]"),
-        Drop(name) =>        format!("[- {name}]"),
-        Info(name, info) => format!("[  {name}]: {info}"),
-        Error(name, error) => format!("[E {name}]: {error}"),
-    }
-    let mut log = INTER_LOG.lock().unwrap();
-    *log = format!( "\n{}\n{}",log, newItem );
-}
-
-pub enum Status {
-    Simple(&str),
-    New(&str),
-    Drop(&str),
-    Info(&str, &str),
-    Error(&str, &str),
-}
-*/
