59db664c61382352a045e5e4455b4801dfc20ae5
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-02 01:27:19 +0300

    Update audio_device.rs

--- src/audio/audio_device.rs
+++ src/audio/audio_device.rs
@@ -11,9 +11,6 @@ use rustysynth::*;
 //mod midi_sequencer;
 use crate::proxy_render::*;
 
-static SF_PIANO:   &'static [u8] = include_bytes!("../SoundFonts/Piano Grand.SF2");
-//static SF_STRINGS: &'static [u8] = include_bytes!("../SoundFonts/String Marcato.SF2");
-//static SF_ORGAN:   &'static [u8] = include_bytes!("../SoundFonts/Organ Chorus.SF2");
 
 
 // tinyaudio wrapper
