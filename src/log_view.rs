17715c997c32ca50485a7ba105875585fb7e1f01
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-01 17:44:08 +0300

    Update log_view.rs

--- src/log_view.rs
+++ src/log_view.rs
@@ -1,2 +1,15 @@
+use crate::raadbg::log;
 
+pub struct LogView {
+}
+
+impl LogView {
+    fn new() -> Self {
+        Self {}
+    }
+
+    pub fn updateUI(&mut self, ui: &mut egui::Ui ) {
+            ui.label( log::get() );
+    }
+}
 
