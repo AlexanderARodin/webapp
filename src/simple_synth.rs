8c841fe290e232868ef0e14990ecb0c66abece11
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-01 23:34:59 +0300

    Create simple_synth.rs

---
+++ src/simple_synth.rs
@@ -0,0 +1,28 @@
+
+use crate::raadbg::log;
+
+
+
+pub struct SimpleSynth{
+    clck: f32,
+}
+impl Default for SimpleSynth {
+    fn default() -> Self {
+        Self::new( 44100, 441*2 )
+    }
+}
+impl Drop for SimpleSynth {
+    fn drop(&mut self) {
+        self.stop();
+        log::drop("SimpleSynth");
+    }
+}
+impl SimpleSynth {
+    pub fn new( sample_rate: usize, block_size: usize ) -> Self {
+        log::create("SimpleSynth");
+        Self{ 
+            clck: 0_f32
+        }
+    }
+}
+
