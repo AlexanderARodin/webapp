90b108204b3f4b2d7647c7820a70256138424c84
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-02 01:27:27 +0300

    Update audio.rs

--- src/audio.rs
+++ src/audio.rs
@@ -1,2 +1,7 @@
 
 mod audio_device;
+
+
+static SF_PIANO:   &'static [u8] = include_bytes!("../SoundFonts/Piano Grand.SF2");
+//static SF_STRINGS: &'static [u8] = include_bytes!("../SoundFonts/String Marcato.SF2");
+//static SF_ORGAN:   &'static [u8] = include_bytes!("../SoundFonts/Organ Chorus.SF2");
