71a7e429a734a6c7ab8ba946a3059448dde9748e
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-02 02:32:02 +0300

    Update midi_controller.rs

--- src/audio/midi_controller.rs
+++ src/audio/midi_controller.rs
@@ -1,6 +1,6 @@
 
 
-trait MidiController {
+pub trait MidiController {
     fn reset(&mut self) {}
     fn note_on(&mut self, channel: i32, key: i32, velocity: i32) {}
     fn note_off(&mut self, channel: i32, key: i32) {}
