ebfaf23ddb77c06a2397e8747991be7c53adbaed
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-03 01:06:03 +0300

    Update proxy_render.rs

--- src/audio/audio_device/proxy_render.rs
+++ src/audio/audio_device/proxy_render.rs
@@ -1,31 +1,23 @@
 use std::sync::{Arc,Mutex};
-use crate::raadbg::log;
-
-//use crate::audio::midi_rx_tx::*;
+//use crate::raadbg::log;
 //  //  //  //  //  //  //  //  //
 
 
 pub struct ProxyRender {
     pub(crate) render_wrapper: Option< Arc<Mutex<dyn super::SoundRender>> >,
 }
-impl Drop for ProxyRender{
-    fn drop(&mut self) {
-        log::drop("ProxyRender");
-    }
-}
-//impl Default for ProxyRender {
-//    fn default() -> Self {
-//        Self::new()
+//impl Drop for ProxyRender{
+//    fn drop(&mut self) {
+//        log::drop("ProxyRender");
 //    }
 //}
 impl ProxyRender {
     pub fn new() -> Arc<Mutex<Self>> {
-        log::create("ProxyRender");
+//        log::create("ProxyRender");
         Arc::new(Mutex::new(Self{ 
             render_wrapper: None
         }))
     }
-
     pub fn render(&mut self, data: &mut [f32]) {
         match &self.render_wrapper {
             None => {
@@ -33,9 +25,9 @@ impl ProxyRender {
                     *sample = 0_f32;
                 }
             },
-            Some(render_wrapper) => {
-                let mut render_wrapper_lock = render_wrapper.lock().expect("can't lock RenderWrapper");
-                render_wrapper_lock.render(data);
+            Some(sound_render) => {
+                let mut sound_render_lock = render_wrapper.lock().expect("FATAL: can't lock SoundRender!");
+                sound_render_lock.render(data);
             }
         }
     }
