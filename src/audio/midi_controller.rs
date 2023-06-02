8026c488f5263036c66afaf02a83b4d740424b91
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-02 17:30:06 +0300

    Update midi_controller.rs

--- src/audio/midi_controller.rs
+++ src/audio/midi_controller.rs
@@ -6,8 +6,8 @@ pub trait MidiReceiver {
 }
 
 pub trait MidiSender {
-    fn invoke_reset(&mut self)
-    fn invoke_midi_command(&mut self, channel: i32, command: i32, data1: i32, data2: i32)
+    fn invoke_reset(&mut self);
+    fn invoke_midi_command(&mut self, channel: i32, command: i32, data1: i32, data2: i32);
     
     fn invoke_note_on(&mut self, channel: i32, key: i32, velocity: i32) {
         self.invoke_midi_command( channel, 0x90, key, velocity );
