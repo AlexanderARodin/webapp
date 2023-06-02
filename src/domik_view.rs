5d0dafcb376fea92284b278236c59c164c6162d1
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-02 19:13:07 +0300

    Update domik_view.rs

--- src/domik_view.rs
+++ src/domik_view.rs
@@ -23,24 +23,34 @@ impl DomikView {
                     audio_device: &mut AudioDevice ) {
             ui.label( format!("WWWapp Template {}", VERS) );
             ui.separator();
-            let btn = ui.button( format!("audio status = {}", audio_device.is_started()) );
-            if btn.clicked(){
-                let _res = audio_device.start();
-            }
-            let btnStop = ui.button( "stop" );
-            if btnStop.clicked(){
-                audio_device.stop();
-            }
+            ui.horizontal( |ui| {
+                    let btn = ui.button(
+                        format!("START! [status = {}]", 
+                        audio_device.is_started()) 
+                        );
+                    if btn.clicked(){
+                        let _res = audio_device.start();
+                    }
+                    let btnStop = ui.button( "STOP" );
+                    if btnStop.clicked(){
+                        audio_device.stop();
+                    }
+                })
             ui.separator();
-
-            let btnA = ui.button( "note ON" );
-            if btnA.clicked(){
-                audio_device.invoke_note_on(0,0,0);
-            }
-            let btnB = ui.button( "note OFF" );
-            if btnB.clicked(){
-                audio_device.invoke_note_off(2,0);
-            }
+            ui.horizontal( |ui| {
+                    let btnA = ui.button( "note ON" );
+                    if btnA.clicked(){
+                        audio_device.invoke_note_on(1,60,0);
+                    }
+                    let btnA2 = ui.button( "note ON2" );
+                    if btnA2.clicked(){
+                        audio_device.invoke_note_on(1,72,0);
+                    }
+                    let btnB = ui.button( "note OFF" );
+                    if btnB.clicked(){
+                        audio_device.invoke_note_off(1,60);
+                    }
+                })
             ui.separator();
             let alt_btn = ui.button( "invoke RESET" );
             if alt_btn.clicked(){
