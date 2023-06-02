3f28b4b815e7403f727b1b3ffb7d8846e9ce7f48
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-03 02:44:17 +0300

    Update domik_view.rs

--- src/domik_view.rs
+++ src/domik_view.rs
@@ -68,7 +68,7 @@ impl DomikView {
                         let simsyn = SimpleSynth::new( settings.sample_rate );
                         let mut dt: [f32; 10] = [0_f32; 10];
                         simsyn.render(&mut dt);
-                        let simsyn_wrapper: Arc<Mutex = Arc::new(Mutex::new( simsyn ));
+                        let simsyn_wrapper: = Arc::new(Mutex::new( simsyn ));
                         audio_device.set_soundrender( Some(simsyn_wrapper) );
                     }
                     let btnRA = ui.button( "RustySynt - A" );
