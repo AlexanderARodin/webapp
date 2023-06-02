b344a30f90fe7b6a97d9076a591d2a76d459ab5e
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-02 17:27:30 +0300

    Update midi_controller.rs

--- src/audio/midi_controller.rs
+++ src/audio/midi_controller.rs
@@ -10,10 +10,10 @@ pub trait MidiSender {
     pub fn invoke_midi_command(&mut self, channel: i32, command: i32, data1: i32, data2: i32)
     
     pub fn invoke_note_on(&mut self, channel: i32, key: i32, velocity: i32) {
-        self.invore_midi_command( channel, 0x90, key, velocity );
+        self.invoke_midi_command( channel, 0x90, key, velocity );
     }
     pub fn invoke_note_off(&mut self, channel: i32, key: i32) {
-        self.invore_midi_command( channel, 0x80, key );
+        self.invoke_midi_command( channel, 0x80, key );
     }
 }
 
