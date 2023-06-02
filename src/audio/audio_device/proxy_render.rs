f95494efbe438dd898dec9bfed21cc08081df7bb
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-03 00:55:58 +0300

    Update proxy_render.rs

--- src/audio/audio_device/proxy_render.rs
+++ src/audio/audio_device/proxy_render.rs
@@ -15,14 +15,14 @@ impl Drop for ProxyRender{
 }
 impl Default for ProxyRender {
     fn default() -> Self {
-        Self::new()//None)
+        Self::new()
     }
 }
 impl ProxyRender {
-    fn new( /*render_wrapper: Option< Arc<Mutex<dyn super::SoundRender>> >*/ ) -> Self {
+    fn new() -> Self {
         log::create("ProxyRender");
         Self{ 
-            render_wrapper: None //render_wrapper
+            render_wrapper: None
         }
     }
 
