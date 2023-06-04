d88e691c235f12247659ed3b0e0f55dceb055ec4
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-05 00:47:37 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ use raadbg::log;
 #[ cfg(not(target_arch = "wasm32")) ]
 fn main() -> Result<(), eframe::Error> {
     log::simple("MAIN has beed entered..");
-    ttt();
 
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(200., 300.)),
@@ -39,7 +38,6 @@ fn main() {
     tracing_wasm::set_as_global_default();
 
     let options = eframe::WebOptions::default();
-    ttt();
 
     wasm_bindgen_futures::spawn_local(async {
         eframe::start_web(
@@ -52,31 +50,3 @@ fn main() {
     });
 }
 
-
-
-use tinyaudio::prelude::*;
-
-fn ttt() {
-    let params = OutputDeviceParameters {
-        channels_count: 2,
-        sample_rate: 44100,
-        channel_sample_count: 4410,
-    };
-
-    let _device = run_output_device(params, {
-        let mut clock = 0f32;
-        move |data| {
-            for samples in data.chunks_mut(params.channels_count) {
-                clock = (clock + 1.0) % params.sample_rate as f32;
-                let value =
-                    (clock * 440.0 * 2.0 * std::f32::consts::PI / params.sample_rate as f32).sin();
-                for sample in samples {
-                    *sample = value;
-                }
-            }
-        }
-    })
-    .unwrap();
-
-    //std::thread::sleep(std::time::Duration::from_secs(5));
-}
