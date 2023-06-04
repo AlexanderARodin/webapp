92f85926e2b7b194f892043ea34b2fa422b8349d
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-05 00:51:31 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,27 @@ use raadbg::log;
 #[ cfg(not(target_arch = "wasm32")) ]
 fn main() -> Result<(), eframe::Error> {
     log::simple("MAIN has beed entered..");
+    
+    let params = OutputDeviceParameters {
+        channels_count: 2,
+        sample_rate: 44100,
+        channel_sample_count: 4410,
+    };
+
+    let _device = run_output_device(params, {
+        let mut clock = 0f32;
+        move |data| {
+            for samples in data.chunks_mut(params.channels_count) {
+                clock = (clock + 1.0) % params.sample_rate as f32;
+                let value =
+                    (clock * 440.0 * 2.0 * std::f32::consts::PI / params.sample_rate as f32).sin();
+                for sample in samples {
+                    *sample = value;
+                }
+            }
+        }
+    })
+    .unwrap();
 
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(200., 300.)),
@@ -33,6 +54,27 @@ fn main() -> Result<(), eframe::Error> {
 #[ cfg(target_arch = "wasm32") ]
 fn main() {
     log::simple("wasmMAIN has beed entered..");
+    
+    let params = OutputDeviceParameters {
+        channels_count: 2,
+        sample_rate: 44100,
+        channel_sample_count: 4410,
+    };
+
+    let _device = run_output_device(params, {
+        let mut clock = 0f32;
+        move |data| {
+            for samples in data.chunks_mut(params.channels_count) {
+                clock = (clock + 1.0) % params.sample_rate as f32;
+                let value =
+                    (clock * 440.0 * 2.0 * std::f32::consts::PI / params.sample_rate as f32).sin();
+                for sample in samples {
+                    *sample = value;
+                }
+            }
+        }
+    })
+    .unwrap();
 
     console_error_panic_hook::set_once();
     tracing_wasm::set_as_global_default();
@@ -48,5 +90,30 @@ fn main() {
         .await
         .expect("failure with starting EFRAME");
     });
+}
+
+fn ttt() {
+    let params = OutputDeviceParameters {
+        channels_count: 2,
+        sample_rate: 44100,
+        channel_sample_count: 4410,
+    };
+
+    let _device = run_output_device(params, {
+        let mut clock = 0f32;
+        move |data| {
+            for samples in data.chunks_mut(params.channels_count) {
+                clock = (clock + 1.0) % params.sample_rate as f32;
+                let value =
+                    (clock * 440.0 * 2.0 * std::f32::consts::PI / params.sample_rate as f32).sin();
+                for sample in samples {
+                    *sample = value;
+                }
+            }
+        }
+    })
+    .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
 
