63b009e5dfc811b4211900d8c7e3ced90eb77995
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-05 00:48:23 +0300

    Update main_app.rs

--- src/main_app.rs
+++ src/main_app.rs
@@ -33,6 +33,7 @@ impl Default for MainApp {
 
 impl MainApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        ttt();
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
@@ -70,5 +71,35 @@ impl eframe::App for MainApp {
                 ui.label( format!("just edited: [{}]", self.txt) );
             });
     }
+}
+
+
+
+
+use tinyaudio::prelude::*;
+
+fn ttt() {
+    let params = OutputDeviceParameters {
+        channels_count: 2,
+        sample_rate: 44100,
+        channel_sample_count: 4410,
+    };
+
+    let _device = run_output_device(params, {
+        let mut clock = 0f32;
+        move |data| {
+            for samples in data.chunks_mut(params.channels_count) {
+                clock = (clock + 1.0) % params.sample_rate as f32;
+                let value =
+                    (clock * 440.0 * 2.0 * std::f32::consts::PI / params.sample_rate as f32).sin();
+                for sample in samples {
+                    *sample = value;
+                }
+            }
+        }
+    })
+    .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
 
