b274b162cbc9fa4e6492086a2ba8d6343d391082
AlexanderARodin <rodin_aa@inbox.ru>
2023-06-06 01:22:59 +0300

    saving

--- src/main_app.rs
+++ src/main_app.rs
@@ -25,7 +25,7 @@ impl Default for MainApp {
         Self {
             txt:"<empty>".to_owned(), pressed:false, 
             log_view: LogView::new(),
-            audio_device: AudioDevice::new( 44100, 441*2 ),
+            audio_device: AudioDevice::new( 44100, 441*4 ),
             domikView: DomikView::new()
         }
     }
