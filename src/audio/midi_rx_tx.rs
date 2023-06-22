82094ae7705907e1fb83ea26cd443edf8e4f064f
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-06-22 14:02:02 +0300

    Update midi_rx_tx.rs

--- src/audio/midi_rx_tx.rs
+++ src/audio/midi_rx_tx.rs
@@ -1,10 +1,5 @@
 
 
-pub trait MidiReceiver {
-    fn reset(&mut self);
-    fn process_midi_command(&mut self, channel: i32, command: i32, data1: i32, data2: i32);
-}
-
 pub trait MidiSender {
     fn invoke_reset(&mut self);
     fn invoke_midi_command(&mut self, channel: i32, command: i32, data1: i32, data2: i32);
