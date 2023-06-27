26bc2f28c5833d265fd70919f3dd85828000881f
AlexanderARodin <rodin_aa@inbox.ru>
2023-06-27 23:54:18 +0300

    saving

--- src/audio/audio_device/proxy_render/mod.rs
+++ src/audio/audio_device/proxy_render/mod.rs
@@ -69,11 +69,5 @@ impl Drop for ProxyRender {
     }
 }
 
-impl MidiReceiver for ProxyRender {
-    fn reset(&mut self) {
-    }
-    fn process_midi_command(&mut self, channel: i32, command: i32, data1: i32, data2: i32) {
-    }
-}
 
 
