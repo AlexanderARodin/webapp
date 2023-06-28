720d9ad2797cc9d888c4c257350ece81c9d07cb8
AlexanderARodin <rodin_aa@inbox.ru>
2023-06-28 22:27:57 +0300

    saving

--- src/audio/audio_device/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod lib;
-pub use lib::*;
-
-pub mod proxy_render;
-mod audio_device_parameters;
-
-pub mod audio_device;
-pub use audio_device::AudioDevice;
-
--- src/audio/mod.rs
+++
@@ -1,5 +0,0 @@
-
-
-//static SF_PIANO:   &'static [u8] = include_bytes!("../../SoundFonts/Piano Grand.SF2");
-//static SF_STRINGS: &'static [u8] = include_bytes!("../../SoundFonts/String Marcato.SF2");
-//static SF_ORGAN:   &'static [u8] = include_bytes!("../../SoundFonts/Organ Chorus.SF2");
